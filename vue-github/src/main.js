a0d3cefdf9c93de81f2c61135a8e46022c321130
Lucas Silva <lucas.silva@tsaaviacao.com.br>
2022-07-14 14:10:01 -0300

    add vuetify

--- vue-github/src/main.js
+++ vue-github/src/main.js
@@ -5,10 +5,12 @@ import { createProvider } from './vue-apollo'
 // Recomendation of babeljs (https://babeljs.io/docs/en/babel-polyfill)
 import 'core-js/stable'; // only stable feature also is possible with only `core-js`
 import 'regenerator-runtime/runtime'; // To ensure that regeneratorRuntime is defined globally
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
   apolloProvider: createProvider(),
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
